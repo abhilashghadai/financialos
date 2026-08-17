4aeee02751905f543d415a2add92d5cd4a5116e6
abhilashghadai <53260660+abhilashghadai@users.noreply.github.com>
2026-08-17 22:13:37 +0530

    Add Financial Engine V1 public API

---
+++ lib/financial-engine/index.ts
@@ -0,0 +1,7 @@
+export * from './types';
+export { analyzeNetWorth } from './networth';
+export { analyzePortfolio } from './portfolio';
+export { analyzeCashFlow } from './cashflow';
+export { analyzeDebt } from './debt';
+export { scoreFinancialHealth } from './scoring';
+export { generateFinancialInsights } from './insights';
